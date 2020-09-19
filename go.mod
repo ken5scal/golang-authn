80ee8389ee9b1e4a37a92b408d8d88b96b1ed1db
Kengo Suzuki <kengoscal@gmail.com>
2020-09-19 23:12:28 +0900

    add rotation
    Signed-off-by: Kengo Suzuki <kengoscal@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/ken5scal/golang-arch
 
 go 1.14
 
-require golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/satori/go.uuid v1.2.0
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
+	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,102 @@ package main
 
 import (
 	"crypto/hmac"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
 
 	"golang.org/x/crypto/bcrypt"
+
+	"github.com/dgrijalva/jwt-go"
 )
+
+type UserClaims struct {
+	jwt.StandardClaims
+	SessionID int64
+}
+
+func (u *UserClaims) Valid() error {
+	if !u.VerifyExpiresAt(time.Now().Unix(), true) {
+		return fmt.Errorf("Token has expired")
+	}
+
+	if u.SessionID == 0 {
+		return fmt.Errorf("Invalid session ID")
+	}
+
+	return nil
+}
+
+func createToken(c *UserClaims) (string, error) {
+	t := jwt.NewWithClaims(jwt.SigningMethodHS512, c)
+	signedToken, err := t.SignedString(keys[currentKid].key)
+	if err != nil {
+		return "", fmt.Errorf("Error in createTOken when signing token: %w", err)
+	}
+	return signedToken, nil
+}
+
+var currentKid = ""
+
+type key struct {
+	key     []byte
+	created time.Time
+}
+
+var keys = map[string]key{} // usually it's in db
+
+func generateNewKey() error {
+	newKey := make([]byte, 64)
+	if _, err := io.ReadFull(rand.Reader, newKey); err != nil {
+		return fmt.Errorf("Error in generateNewKey while genrating simpleKey: %w", err)
+	}
+
+	uid := uuid.NewV4()
+	keys[uid.String()] = key{
+		key:     newKey,
+		created: time.Now(),
+	}
+
+	currentKid = uid.String()
+}
+
+func parseToken(signedToken string) (*UserClaims, error) {
+	t, err := jwt.ParseWithClaims(signedToken, &UserClaims{}, func(t *jwt.Token) (interface{}, error) {
+		if t.Method.Alg() != jwt.SigningMethodHS512.Alg() {
+			return nil, fmt.Errorf("Invalid signing algorithm")
+		}
+
+		kid, ok := t.Header["kid"].(string)
+		if !ok {
+			return nil, fmt.Errorf("Invalid kid")
+		}
+
+		k, ok := keys[kid] // Get from db
+		if !ok {
+			return nil, fmt.Errorf("Invalid simpleKey ID")
+		}
+
+		return k.key, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error in parseToken while parsing token: %w", err)
+	}
+
+	if !t.Valid {
+		return nil, fmt.Errorf("Error in parseToken, token is not valid")
+	}
+
+	return t.Claims.(*UserClaims), nil
+}
 
 type person struct {
 	First string
@@ -20,7 +107,7 @@ func main() {
 	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
 
 	for i := 1; i <= 64; i++ {
-		key = append(key, byte(i))
+		simpleKey = append(simpleKey, byte(i))
 	}
 
 	pass := "abcdef"
@@ -41,10 +128,10 @@ func main() {
 	//http.ListenAndServe(":8080", nil)
 }
 
-var key = []byte{}
+var simpleKey = []byte{}
 
 func signMessage(msg []byte) ([]byte, error) {
-	h := hmac.New(sha256.New, key)
+	h := hmac.New(sha256.New, keys[currentKid].key)
 	if _, err := h.Write(msg); err != nil {
 		return nil, fmt.Errorf("Error in signMessage while hasing msg : %w", err)
 	}
