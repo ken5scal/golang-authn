f35f0533c0e49fa9b4b510f811d7f8d0d3792b94
Kengo Suzuki <kengoscal@gmail.com>
2020-09-24 08:47:48 +0900

    login and register
    Signed-off-by: Kengo Suzuki <kengoscal@gmail.com>

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 
@@ -64,6 +65,7 @@ type key struct {
 }
 
 var keys = map[string]key{} // usually it's in db
+var db = map[string][]byte{}
 
 func generateNewKey() error {
 	newKey := make([]byte, 64)
@@ -192,12 +194,105 @@ func main() {
 
 	log.Println("logged in")
 
-	http.HandleFunc("/", hoge)
+	http.HandleFunc("/hoge", hoge)
 	http.HandleFunc("/submit", fuga)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
 	http.ListenAndServe(":8080", nil)
 	//http.HandleFunc("/encode", foo)
 	//http.HandleFunc("/decode", bar)
 	//http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	msg := r.FormValue("msg")
+	fmt.Fprintf(w, `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+	<h1>If there was any error, here it is: %s</h1>
+	<h1>Register</h1>
+    <form action="/register" method="POST">
+        <input type="email" name="e">
+        <input type="password" name="p">
+        <input type="submit">
+    </form>
+	<h1>Log In</h1>
+    <form action="/login" method="POST">
+        <input type="email" name="e">
+        <input type="password" name="p">
+        <input type="submit">
+    </form>
+</body>
+</html>`, msg)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		errorMsg := url.QueryEscape("your method was not post")
+		http.Redirect(w, r, "/?msg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+	e := r.FormValue("e")
+	if e == "" {
+		errorMsg := url.QueryEscape("your email need to not be empty")
+		http.Redirect(w, r, "/?msg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+	p := r.FormValue("p")
+	if p == "" {
+		errorMsg := url.QueryEscape("your password need to not be empty")
+		http.Redirect(w, r, "/?msg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+
+	bsp, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.DefaultCost)
+	if err != nil {
+		errorMsg := "internal server error"
+		http.Error(w, errorMsg, http.StatusInternalServerError)
+		return
+	}
+	db[e] = bsp
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		msg := url.QueryEscape("your method was not post")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+	e := r.FormValue("e")
+	if e == "" {
+		msg := url.QueryEscape("your email need to not be empty")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+	p := r.FormValue("p")
+	if p == "" {
+		msg := url.QueryEscape("your password need to not be empty")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	if _, ok := db[e]; !ok {
+		msg := url.QueryEscape("your email or password didn't match")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword(db[e], []byte(p)); err != nil {
+		msg := url.QueryEscape("your email or password didn't match")
+		http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	msg := url.QueryEscape("you logged in " + e)
+	http.Redirect(w, r, "/?msg="+msg, http.StatusSeeOther)
 }
 
 var simpleKey = []byte{}
